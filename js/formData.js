cae6ed7869031462049a578c031eeb6a0a20287c
Ryledra <vince@nadin.one>
2018-09-07 13:07:36 +0100

    forename changed to name in formData.js

--- js/formData.js
+++ js/formData.js
@@ -1,7 +1,7 @@
 
 onload = function() {
 
-    document.getElementById("name").innerHTML += localStorage.getItem("forename");
+    document.getElementById("name").innerHTML += localStorage.getItem("name");
     document.getElementById("DofB").innerHTML += localStorage.getItem("DofB");
     document.getElementById("email").innerHTML += localStorage.getItem("email");
     document.getElementById("address").innerHTML += localStorage.getItem("address");
